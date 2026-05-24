456bb52dc05d304902f28475c1118a96b8cb4fbb
NarraAtor <70336628+NarraAtor@users.noreply.github.com>
2026-05-24 12:00:22 -0400

    Fixed accidental file deletion

---
+++ Source/FacepunchSteamworks/FacepunchSteamSettings.cs
@@ -0,0 +1,39 @@
+﻿using FlaxEngine;
+
+namespace FacepunchSteamworks;
+
+/// <summary>
+/// Facepunch steam settings.
+/// </summary>
+public class FacepunchSteamSettings : SettingsBase
+{
+    /// <summary>
+    /// If true, initializes steam during the `FacepunchSteamworksPlugin` initialization.
+    /// </summary>
+    public bool InitializeSteamAutomatically = true;
+
+    /// <summary>
+    /// If true, while not in the editor, the app will reboot if steam is required to start it.
+    /// </summary>
+    public bool RestartAppIfSteamRequires = true;
+
+    /// <summary>
+    /// If true, while the build is not in release mode, the steam library will log callbacks.
+    /// </summary>
+    public bool UseSteamDebugCallback = true;
+
+    /// <summary>
+    /// If true, the steam library will be initialized in the editor.
+    /// </summary>
+    public bool InitializeInEditor = true;
+
+    /// <summary>
+    /// The steam app id. 480 is SpaceWars.
+    /// </summary>
+    public uint AppId = 480;
+
+    /// <summary>
+    /// The host or server steam id to join. Set this using StartClient(ulong)
+    /// </summary>
+    public ulong TargetSteamId = 0;
+}
